7055145acfabe578466a7fff44e88c8572670653
telugu-boy <h0l33fuk@gmail.com>
2025-10-28 18:04:30 -0600

    add failing hasCity test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,7 +1,25 @@
 package com.example.lab08;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
-// test CI on github actions
+    private CustomList cityList;
+
+    @Before
+    public void before() {
+        this.cityList = new CustomList();
+    }
+
+    // adds a city to cityList and checks hasCity for the same city
+    @Test
+    public void Test_hasCity(){
+        City edmonton = new City("Edmonton", "Alberta");
+        City calgary = new City("Calgary", "Alberta");
+
+        cityList.addCity(edmonton);
+
+        assertTrue(cityListadd .hasCity(edmonton));
+        assertFalse(cityList.hasCity(calgary));
+    }
 }
